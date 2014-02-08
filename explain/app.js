7d841d176c6652cd05c8ed56c18d8c988abd7fe5
Daniela Baron <baron.daniela@gmail.com>
2014-02-08 07:54:19 -0500

    change hint to use _id index

--- explain/app.js
+++ explain/app.js
@@ -5,7 +5,9 @@ MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
 
 	var query = {'x':2};
 	var projection = {};
-	var options = { 'hint' : { '$natural' : 1 } };
+	var projection = {};
+	// var options = { 'hint' : { '$natural' : 1 } };
+	var options = { 'hint' : { '_id' : 1 } };
 	var cursor = db.collection('coll').find(query, projection, options);
 	cursor.explain(function(err, explain_output) {
 		console.log(explain_output);
